86f6292e7fa8503264f32ac774450fec9bda14b7
PavelCA04 <bzamora@estudiantec.cr>
2023-10-25 15:14:25 -0600

    Update menu.h

--- structs/menu.h
+++ structs/menu.h
@@ -49,19 +49,21 @@ void limpiarConsola(){
 
 void imprimirBanner(){
     std::string banner =
-"   ██████╗  █████╗ ████████╗████████╗██╗     ███████╗    ███████╗ ██████╗ ██████╗     ████████╗██╗  ██╗███████╗    ███████╗ ██████╗ ██╗   ██╗██╗     ███████╗"
-"██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝    ██╔════╝██╔═══██╗██╔══██╗    ╚══██╔══╝██║  ██║██╔════╝    ██╔════╝██╔═══██╗██║   ██║██║     ██╔════╝"
-"██████╔╝███████║   ██║      ██║   ██║     █████╗      █████╗  ██║   ██║██████╔╝       ██║   ███████║█████╗      ███████╗██║   ██║██║   ██║██║     ███████╗"
-"██╔══██╗██╔══██║   ██║      ██║   ██║     ██╔══╝      ██╔══╝  ██║   ██║██╔══██╗       ██║   ██╔══██║██╔══╝      ╚════██║██║   ██║██║   ██║██║     ╚════██║"
-"██████╔╝██║  ██║   ██║      ██║   ███████╗███████╗    ██║     ╚██████╔╝██║  ██║       ██║   ██║  ██║███████╗    ███████║╚██████╔╝╚██████╔╝███████╗███████║"
-"╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝    ╚═╝      ╚═════╝ ╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
-                                                                                                                                                          
+    "          ______             _     ______                    _        \n"
+    "         / _____)           | |   (____  \\         _     _  | |       \n"
+    "        ( (____   ___  _   _| |    ____)  )_____ _| |_ _| |_| | _____ \n"
+    "         \\____ \\ / _ \\| | | | |   |  __  ((____ (_   _|_   _) || ___ |\n"
+    "         _____) ) |_| | |_| | |   | |__)  ) ___ | | |_  | |_| || ____|\n"
+    "        (______/ \\___/|____/ \\_)  |______/\\_____|  \\__)  \\__)\\__)_____)\n"
+    "                                                                      \n"     
+                                                   
+                 
         "  `._`-._                                           _,-',\n"
         "     `._ `-._        __.-----.__        _,-'  _,\n"
         "        `._  `-._/__.-' `._.-'__.-'__.-'  _, '\n"
         "           `._  `-.__  ._,-'`-._,_.-'  _, '\n"
         "              `._  `#===           ===#'  _, '\n"
-        "               `._/)  ._               _.  (\_, '\n"
+        "               `._/)  ._               _.  (\", '\n"
         "               )*'     **.__     __.**     '*(\n"
         "               #  .==..__  \"\"   \"\"  __..==,  #\n"
         "               #   `\"._(_).       .(_)_.\"'   # ";
@@ -86,7 +88,7 @@ void menu(){
     imprimirBanner();
     int opcion = 0;
     while (opcion != 8){
-        cout << textoAzul("Menu Principal") << endl;
+        cout << textoAzul("Main Menu") << endl;
         imprimirOpciones();
         opcion = pedirNumero("Selection: ");
         switch (opcion){
@@ -133,26 +135,20 @@ void menu(){
 
 void menuWorld(){
     int opcion = 0;
-    while (opcion != 3){
+    while (opcion != 2){
         limpiarConsola();
         cout << textoAzul("World Menu") << endl;
-        cout << " (1) Create Humans" << endl;
-        cout << " (2) Return to main menu  " << endl;
+        cout << " (1) Generate Humanity" << endl;
+        cout << " (2) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
             case 1: // Ver clientes
                 limpiarConsola();
-                cout << textoAzul("Clientes") << endl;
-                imprimirClientes(clientes);
+                cout << textoAzul("Generate Humanity") << endl;
+                //Funcion to generate humanity
                 continuar();
                 break;
-            case 2: // Agregar Clientes
-                limpiarConsola();
-                cout << textoAzul("Agregar Cliente") << endl;
-                agregarClientes(clientes);
-                continuar();
-                break;
-            case 3:
+            case 2:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -163,31 +159,39 @@ void menuWorld(){
 
 
 
-
-
 void menuTree(){
     int opcion = 0;
-    while (opcion != 3){
+    while (opcion != 5){
         limpiarConsola();
         cout << textoAzul("Tree of Souls Menu") << endl;
         cout << " (1) Print amount of tree levels" << endl;
         cout << " (2) Print amount of nodes" << endl;
         cout << " (3) Print amount of humans in tree  " << endl;
         cout << " (4) Print amount of humans ask  " << endl;
-        cout << " (5) Return to main menu  " << endl;
+        cout << " (5) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
-            case 1: // Imprimir Pedidos
+            case 1: // Print amount of tree levels
                 limpiarConsola();
-                cout << textoAzul("Pedidos") << endl;
-                imprimirPedidos(prioridadAlta, prioridadMedia, prioridadBaja);
+                cout << textoAzul("Tree Levels") << endl;
+                //Funcion to print amount of tree levels
                 break;
-            case 2: // Agregar pedido
+            case 2: // Print amount of nodes
                 limpiarConsola();
-                cout << textoAzul("Agregar Pedido") << endl;;
-                agregarPedido(clientes, productos, prioridadAlta);
+                cout << textoAzul("Tree Nodes") << endl;;
+                // Funcion to print amount of nodes
                 break;
-            case 3:
+            case 3: // Print amount of humans in tree
+                limpiarConsola();
+                cout << textoAzul("Humans in Tree") << endl;;
+                // Funcion to print amount of humans in tree
+                break;
+            case 4: // Print humans ask teacher
+                limpiarConsola();
+                cout << textoAzul("Humans") << endl;
+                // Funcion to print humans ask teacher
+                break;
+            case 5:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -201,23 +205,33 @@ void menuTree(){
 
 void menuSocialMedia(){
     int opcion = 0;
-    while (opcion != 3){
+    while (opcion != 5){
         limpiarConsola();
         cout << textoAzul("Menu Social Media Publications") << endl;;
         cout << " (1) Look for a user and create a post in a certain social media app" << endl;
-        cout << " (2) Region post" << endl;
-        cout << " (3) Profesion post in multiple apps" << endl;
-        cout << " (4) Family post in favorite apps" << endl;
-        cout << " (5) Return to main menu  " << endl;
+        cout << " (2) Region Post" << endl;
+        cout << " (3) Profesion Post in multiple apps" << endl;
+        cout << " (4) Family Post in favorite apps" << endl;
+        cout << " (5) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
-            case 1: // Encender Balanceador
-                balanceador->Reanudar();
+            case 1: // 
+                limpiarConsola();
+                cout << textoAzul("User ID") << endl;
                 break;
-            case 2: // Apagar Balanceador
-                balanceador->Pausar();
+            case 2: //
+                limpiarConsola();
+                cout << textoAzul("Region") << endl;
                 break;
             case 3:
+                limpiarConsola();
+                cout << textoAzul("Profesion") << endl;
+                break;
+            case 4:
+                limpiarConsola();
+                cout << textoAzul("Family Post") << endl;
+                break;
+            case 5:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -228,7 +242,7 @@ void menuSocialMedia(){
 
 void menuHell(){
     int opcion = 0;
-    while (opcion != 4){
+    while (opcion != 2){
         limpiarConsola();
         cout << textoAzul("Hell Menu") << endl;
         cout << textoRojo(" (1) Condemn the sinners with a demon") << endl;
@@ -237,26 +251,10 @@ void menuHell(){
         switch (opcion){
             case 1:
                 limpiarConsola();
-                cout << textoAzul("Productos a Fabricar") << endl;
-                imprimirProductosAFabricar(robots[0]->productos ,robots[0]->productosAFabricar);
-                continuar();
+                cout << textoRojo("Demon of choice") << endl;
+                //Function to condemn a sinner
                 break;
-            case 2: // Imprimir Fabricadores
-                limpiarConsola();
-                cout << textoAzul("Fabricadores") << endl;
-                imprimirFabricadores(robots);
-                continuar();
-                break;
-            case 3:{
-                int robotId = pedirNumero("ID Robot: ");
-                while (!(robotId > 0 && robotId < 11 )){
-                    cout << "ID Inválido" << endl;
-                    robotId = pedirNumero("ID Robot: ");
-                }
-                menuFabricador(robots, robotId-1);
-                break;
-            }
-            case 4:
+            case 2:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -265,11 +263,9 @@ void menuHell(){
 }
 
 
-
-
 void menuHeaven(){
     int opcion = 0;
-    while (opcion != 5){
+    while (opcion != 2){
         limpiarConsola();
         cout << textoAzul("Heaven Menu") << endl;
         cout << " (1) Print Heaven" << endl;
@@ -277,24 +273,12 @@ void menuHeaven(){
         opcion = pedirNumero("Selection: ");
         switch (opcion){
             case 1:{
-                cout << " (A): Categoría A\n (B): Categoría B\n (C): Categoría C\n (D): Todas las categorías" << endl;
-                string tipo = pedirString("Ingrese la categoría: ");
-                while(tipo != "A" && tipo != "B" && tipo != "C" && tipo != "D"){
-                    tipo = pedirString("Ingrese el Tipo: ");
-                }
-                robots[id]->categoria = tipo;
+                limpiarConsola();
+                cout << textoAzul("Heaven") << endl;
+                //Function to print heaven
                 break;
             }
             case 2:
-                robots[id]->prioridad = !robots[id]->prioridad;
-                break;
-            case 3:
-                robots[id]->Reanudar();
-                break;
-            case 4:
-                robots[id]->Pausar();
-                break;
-            case 5:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -304,42 +288,26 @@ void menuHeaven(){
 
 void menuHuman(){
     int opcion = 0;
-    while (opcion != 5){
+    while (opcion != 3){
         limpiarConsola();
         cout << textoAzul("Human Menu") << endl;
         cout << " (1) Search Human through ID" << endl;
         cout << " (2) Search Human through Name and Last Name" << endl;
-        cout << " (3) Return to main menu  " << endl;
+        cout << " (3) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
             case 1:
                 limpiarConsola();
-                cout << textoAzul("Productos a Alistar") << endl;
-                imprimirProductosAAlistar(picking);
-                continuar();
+                cout << textoAzul("Human ID") << endl;
+                //Function to search human through ID
                 break;
             case 2:
                 limpiarConsola();
-                cout << textoAzul("Alistadores") << endl;
-                imprimirAlistadores(picking);
+                cout << textoAzul("Human Name and Last Name") << endl;
+                //Function to search human through Name and Last Name
                 continuar();
                 break;
-            case 3: // Imprimir Alistadores
-                limpiarConsola();
-                cout << textoAzul("Lista espera Alistadores") << endl;
-                imprimirListaEsperaAlistadores(picking->filaAlistadores);
-                continuar();
-                break;
-            case 4:{
-                int alistadorId = pedirNumero("ID Alistador: ");
-                while (!(alistadorId > 0 && alistadorId < 7 )){
-                    cout << "ID Inválido" << endl;
-                    alistadorId = pedirNumero("ID Alistador: ");
-                }
-                menuAlistador(picking->alistadores[alistadorId]);
-                break;
-            }
-            case 5:
+            case 3:
                 return;
             default:
                 cout << "Option unavailable, please select a valid option" << endl;
@@ -356,14 +324,19 @@ void menuWinner(){
         cout << textoAzul("Winner menu") << endl;
         cout << " (1) Who won the battle?" << endl;
         cout << " (2) Total of humans, in Heaven,Hell or Alive" << endl;
-        cout << " (3) Return to main menu  " << endl;
+        cout << " (3) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
-            case 1: // Encender Alistador
-                alistador->Reanudar();
+            case 1: 
+                limpiarConsola();
+                cout << textoAzul("Winner") << endl;
+                //Function to print winner
+                continuar();
                 break;
-            case 2: // Apagar Alistador
-                alistador->Pausar();
+            case 2: 
+                limpiarConsola();
+                cout << textoAzul("Total of humans") << endl;
+                //Function to print total of humans
                 break;
             case 3:
                 return;
@@ -384,20 +357,24 @@ void menuDemons(){
         cout << " (1) Display Demons and their sin" << endl;
         cout << " (2) Human Sinners" << endl;
         cout << " (3) Display list of sinners" << endl;
-        cout << " (4) Return to main menu  " << endl;
+        cout << " (4) Return to Main Menu  " << endl;
         opcion = pedirNumero("Selection: ");
         switch (opcion){
             case 1:
                 limpiarConsola();
-                cout << textoAzul("Pedidos a facturar") << endl;
-                imprimirPedidoAFacturar(facturador->pedidosParaFacturar);
+                cout << textoRojo("Demons") << endl;
+                //Function to print demons
                 continuar();
                 break;
-            case 2: // Encender Facturador
-                facturador->Reanudar();
+            case 2: 
+                limpiarConsola();
+                cout << textoRojo("Human Sinners") << endl;
+                //Function to print human sinners
                 break;
-            case 3: // Apagar Facturador
-                facturador->Pausar();
+            case 3: 
+                limpiarConsola();
+                cout << textoRojo("List of sinners") << endl;
+                //Function to print list of sinners
                 break;
             case 4:
                 return;
